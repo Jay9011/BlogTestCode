783e793a6763d5e8b5e39ad1fb970fde73c1bb9e
Jay9011 <whdtjq5904@gmail.com>
2023-10-06 20:03:03 +0900

    Lvalue와 Rvalue

--- BlogTestCode/Rvalue_Lvalue/rvalueLiteral.cpp
+++ BlogTestCode/Rvalue_Lvalue/rvalueLiteral.cpp
@@ -1,7 +1,33 @@
 ﻿#include <iostream>
 using namespace std;
+
+int rtnInt() { return 1; }
+int& refInt() {
+	static int a = 10;
+	return a;
+}
+int* rtnPtr()
+{
+	static int a = 10;
+	return &a;
+}
+
 int main()
 {
-	int a = 10;
-	int b = 10;
+	int num = rtnInt();
+
+	int arr[5];
+	arr[0] = 1;
+	int a = arr[0];
+
+	int* ptr = &a;
+	*ptr = 30;
+	int b = *ptr;
+
+	int* p = arr;
+
+	refInt() = 20;
+	*rtnPtr() = 20;
+	cout << refInt() << endl;
+	cout << *rtnPtr() << endl;
 }
